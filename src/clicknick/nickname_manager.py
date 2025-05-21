188cd79fe63a6f77af4040a20c9a31b05d3a13e0
ssweber <57631333+ssweber@users.noreply.github.com>
2025-05-21 09:54:39 -0400

    fix: Address prefix handling
    lower() to correctly match

--- src/clicknick/nickname_manager.py
+++ src/clicknick/nickname_manager.py
@@ -141,7 +141,10 @@ class NicknameManager:
             bool: True if the input is a valid address or numeric value, False otherwise
         """
         # Check if the input is a valid address with correct prefix
+        input_text = input_text.lower()
+        
         for prefix in DATA_TYPES.keys():
+            prefix = prefix.lower()
             if input_text.startswith(prefix) and input_text[len(prefix) :].isdigit():
                 return True
 
